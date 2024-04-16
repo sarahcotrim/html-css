b8b08ea2887803406473d2770d4ebe25b7eaf591
sarahcotrim <145464203+sarahcotrim@users.noreply.github.com>
2024-04-16 09:55:49 -0300

    Add files via upload

---
+++ sistema-aluno/testealuno.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <?php
+    echo $_GET['nome'];
+    echo "<br>";
+    echo $_GET['endereco'];
+    echo "<br>";
+    echo $_GET['nascimento'];
+    echo "<br>";
+    echo $_GET['idade'];
+    echo "<br>";
+    echo $_GET['email'];
+    echo "<br>";
+    echo $_GET['senha'];
+    echo "<br>";
+    echo $_GET['sexo'];
+    ?>
+</body>
+</html>
